e57c3770fe75be9314d122ca47004e5f767b187f
Aditya Goel <adityagoel237@gmail.com>
2023-04-28 03:51:35 +0000

    Script to grab BLS appointment

--- blsScript.py
+++ blsScript.py
@@ -20,40 +20,13 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 browser = webdriver.Chrome('/usr/bin/chromedriver', options=chrome_options)
 browser.get("https://www.blsindia-canada.com/appointmentbls/appointment.php")
 
-# Assign useful elements
+# Assign useful elements to variables
 locationDropdown = Select(browser.find_element(By.ID, 'location'))
 serviceTypeDropdown = Select(browser.find_element(By.ID, 'service_type'))
 appDateTextField = browser.find_element(By.ID, 'app_date')
 
-# Define a function to check availability of appointments:
-def checkAppointments():
 
-  for i in range(1,7):        #Six rows for six weeks (Week[-1] + Week[1-4] + Week[+1]) is displayed
-    for j in range(1,8):      # Seven days pwer week
-      
-      xpath = "//html/body/div[5]/div[1]/table/tbody/tr[" + str(i) + "]" + "/td[" + str(j) +"]"
-      dateClass = browser.find_element(By.XPATH, xpath)
-      print(dateClass.get_attribute("class"))
-
-      if (dateClass.get_attribute("class") == "new day activeClass"):
-      
-        # Define email content
-        email = MIMEMultipart()
-        email['From'] = 'boundlesscuriosity237@gmail.com'
-        email['To'] = 'adityagoel237@gmail.com'
-        email['Subject'] = 'Test Email'
-        body = 'There is an appointment available'
-        email.attach(MIMEText(body, 'plain'))
-
-        # Send email
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login('boundlesscuriosity237@gmail.com', 'yuiiulcvxxnnzblb')
-        text = email.as_string()
-        server.sendmail('boundlesscuriosity237@gmail.com', 'adityagoel237@gmail.com', text)
-
-        server.quit()
-
+# Operations Structure
 def performOperations(blsOffice):
     locationDropdown.select_by_visible_text(blsOffice)
     serviceTypeDropdown.select_by_visible_text("Passport")
@@ -67,6 +40,43 @@ def performOperations(blsOffice):
     nextButton.click()
     checkAppointments()
 
+
+# Functions
+
+# Function to check availability of appointments:
+def checkAppointments():
+
+  for i in range(1,7):        #Six rows for six weeks (Week[-1] + Week[1-4] + Week[+1]) is displayed
+    for j in range(1,8):      # Seven days pwer week
+      
+      dateXPath = "//html/body/div[5]/div[1]/table/tbody/tr[" + str(i) + "]" + "/td[" + str(j) +"]"
+      dateClass = browser.find_element(By.XPATH, dateXPath)
+      print(dateClass.get_attribute("class"))
+
+      if (dateClass.get_attribute("class") == "new day activeClass"):
+        notifyMe()
+
+# Function to email
+def notifyMe():
+    # Define email content
+    email = MIMEMultipart()
+    email['From'] = 'boundlesscuriosity237@gmail.com'
+    email['To'] = 'adityagoel237@gmail.com'
+    email['Subject'] = 'Test Email'
+    body = 'There is an appointment available'
+    email.attach(MIMEText(body, 'plain'))
+
+    # Send email
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('boundlesscuriosity237@gmail.com', 'yuiiulcvxxnnzblb')
+    text = email.as_string()
+    server.sendmail('boundlesscuriosity237@gmail.com', 'adityagoel237@gmail.com', text)
+
+    server.quit()
+
+
+# Main Function
 performOperations("Brampton")
 performOperations("Toronto")
 
